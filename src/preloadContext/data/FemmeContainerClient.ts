5f9a9644bfbb93482c86bda3bf64ee3da655026d
Zachary Hanham <z.hanham00@gmail.com>
2021-08-03 12:46:35 -0400

    Fixed container/account name .env

--- src/preloadContext/data/FemmeContainerClient.ts
+++ src/preloadContext/data/FemmeContainerClient.ts
@@ -1,14 +1,15 @@
 import {BlobServiceClient, StorageSharedKeyCredential} from '@azure/storage-blob';
-const containerName = process.env.ACCOUNT_NAME;
+const containerName = process.env.CONTAINER_NAME;
+const accountName = process.env.ACCOUNT_NAME;
 const accountKey = process.env.ACCOUNT_KEY;
 
-if (!containerName || !accountKey) {
+if (!containerName || !accountKey || !accountName) {
   throw Error('.env is not loaded');
 }
 
-const blobUrl = `https://${containerName}.blob.core.windows.net`;
+const blobUrl = `https://${accountName}.blob.core.windows.net`;
 
-const sharedKeyCredential = new StorageSharedKeyCredential(containerName, accountKey);
+const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey);
 
 const blobServiceClient = new BlobServiceClient(
   // When using AnonymousCredential, following url should include a valid SAS or support public access
